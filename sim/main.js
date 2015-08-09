73ab4b00ad96a7b71e6c517e58365b740f5ee04b
Takeshi Nishikawa <drafear@ring.box2.kmc.gr.jp>
2015-08-09 11:14:29 +0900

    sim - eeeeei

--- sim/main.js
+++ sim/main.js
@@ -404,7 +404,7 @@ function command(str_cmd) {
 				var reps = 0;
 				while (1) {
 					if ( c.match(spells[j]) ) {
-						c = c.replace(spells[j], pell);
+						c = c.replace(new RegExp(".*?"+spells[j]), pell);
 						++reps;
 					}
 					else break;
